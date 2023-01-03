1fbfbaea5beb9b66b7e5c5feea88b8491089b62d
loganeworth <loganeworth@gmail.com>
2023-01-03 15:55:30 -0600

    added a count down timer when quiz starts

--- Develop/script.js
+++ Develop/script.js
@@ -20,34 +20,34 @@ let questions = [
         correctAnswer: '3'
     },
     {
-        question: "Which organelles of a cell are referred to as 'the powerhouse of the cell?'",
+        question: "What organelles of a cell are referred to as 'the powerhouse of the cell?'",
         answers: {
             a: 'Nucleus',
             b: 'Cytoplasm',
             c: 'Chloroplast',
             d: 'Mitochondria',
         },
-        correctAnswers: 'Mitochondria'
+        correctAnswer: 'Mitochondria'
     },
     {
-        question: "",
+        question: "What is the capitol of Minnesota",
         answers: {
-            a: '',
-            b: '',
-            c: '',
-            d: '',
+            a: 'Duluth',
+            b: 'Minneapolis',
+            c: 'St. Paul',
+            d: 'Rochester',
         },
-        correctAnswer: ''
+        correctAnswer: 'St. Paul'
     },
     {
-        question: '',
+        question: 'What is the smallest state in the continental United States?',
         answers: {
-            a: '',
-            b: '',
-            c: '',
-            d: '',
+            a: 'Connecticut',
+            b: 'Hawaii',
+            c: 'Delaware',
+            d: 'Rhode Island',
         },
-        correctAnswer: ''
+        correctAnswer: 'Rhode Island'
     }
     
 ]
@@ -57,7 +57,7 @@ console.log(questions[0].answers.a)
 console.log(questions[0].answers.b)
 console.log(questions[0].answers.c)
 console.log(questions[0].answers.d)
-    var currentQuestion = 0
+    let currentQuestion = 0
 function displayQuestion() {
     let displayQuestionEl = document.querySelector('.displayQuestion')
     let displayAnswer1 = document.querySelector('.displayAnswer1')
@@ -77,7 +77,7 @@ function displayQuestion() {
 function checkAnswer(event) {
     event.preventDefault()
     console.log(event.target.innerText)
-    var correctAnswer = questions[currentQuestion].correctAnswer
+    let correctAnswer = questions[currentQuestion].correctAnswer
     if (correctAnswer === event.target.innerText) {
         console.log('correct')
         if ((questions.length - 1) > currentQuestion) {
@@ -106,7 +106,68 @@ function toggleQuestion1() {
     // else {
     //     Question1.style.display = 'none';
     // }
+    var timeLeft = 30;
+    var elem = document.querySelector('.timer');
+    
+    var timerId = setInterval(countdown, 1000);
+    
+    function countdown() {
+      if (timeLeft == -1) {
+        clearTimeout(timerId);
+        doSomething();
+      } else {
+        elem.innerHTML = timeLeft + ' seconds remaining';
+        timeLeft--;
+      }
+    }
+
+
 }
+
+
+
+
+
+
+// const time = document.querySelector('.timer');
+// let timeSecond = 30;
+
+// displayTime(timeSecond)
+
+// const countDown = setInterval (()=>{
+//     timeSecond--;
+//     displayTime(timeSecond);
+//     if(timeSecond <= 0 || timeSecond < 1){
+//         clearInterval(countDown)
+//     }
+// },1000)
+
+// function displayTime(second) {
+//     const min = Math.floor(second / 60);
+//     const sec = Math.floor(second % 60);
+//     time.innerHTML = `${min<10 ? '0': ''}${min}:${sec<10?'0':''}${sec}`
+// }
+
+
+
+
+// time.innerHTML = `00:${timeSecond}`;
+
+// const countDown = setInterval (()=>{
+//     timeSecond--;
+//     time.innerHTML = `00:${timeSecond}`;
+//     if(timeSecond<0 || timeSecond<1);
+// },)
+
+// let count = 30;
+// let interval = setInterval(function(){
+//     document.getElementById('start-btn');
+//     count--;
+//     if (count === 0) {
+//         clearInterval(interval);
+        
+//     }
+// }, 30);
 
 console.log(toggleQuestion1)
 
